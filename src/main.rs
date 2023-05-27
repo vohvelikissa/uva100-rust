47f7f31de69f68f37e8a14ede3948eda71bde090
Pessi <pessiv@outlook.com>
2023-05-28 01:57:34 +0300

    the algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+fn trenplusone_vec(nnumber:i32) -> Vec<i32> {
+    let mut trenlist:Vec<i32> = Vec::new();
+    let mut current_number = nnumber;
+    while current_number >= 1 {
+        println!("{}",current_number);
+        if current_number%2==0 {
+            current_number = current_number/2;
+        } else {
+            current_number = 3 * current_number + 1;
+        }
+        trenlist.push(current_number);
+        if current_number == 1 {
+            return trenlist;
+        }
+    }
+    trenlist
+}
 fn main() {
-    println!("Hello, world!");
+    println!("{}",trenplusone_vec(99).len()+1);
 }
